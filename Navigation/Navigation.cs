e72f0f26c28152bf19811989c061229743b2baf6
Diones <kleitondionesvr@gmail.com>
2025-08-10 15:29:00 -0300

    Adicionado namespace em Navigation

--- Navigation/Navigation.cs
+++ Navigation/Navigation.cs
@@ -1,18 +1,21 @@
 using UnityEngine;
 
-public class Navigation : MonoBehaviour
+namespace Common.WebGL.Network
 {
+    public class Navigation : MonoBehaviour
+    {
 #if UNITY_WEBGL && !UNITY_EDITOR
     [DllImport("__Internal")]
     private static extern void LoadURL(string url);
 #endif
 
-    public static void GoToPage(string url)
-    {
+        public static void GoToPage(string url)
+        {
 #if UNITY_WEBGL && !UNITY_EDITOR
         LoadURL(url);
 #else
-        Debug.Log("Ir para: " + url);
+            Debug.Log("Ir para: " + url);
 #endif
+        }
     }
 }
